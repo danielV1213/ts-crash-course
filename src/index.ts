b40e306019669d5b5cc935a173b768201fb25936
Daniel Andŕes Vélez Vargas <daniel_andres.velez@uao.edu.co>
2024-05-31 18:05:41 -0500

    [ADD] Arrays

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-let testDocument = 123456789;
+let numbers: number[] = [1, 2, 3];
 
-function printDocument(document: number) {
-  console.log(document);
-};
+// What if the array was empty
+let newArray = [];
 
-printDocument(testDocument);
+// You could append any type of variables to it because it is not expecting a particular type.
+newArray[0] = "1";
+newArray[1] = 2;
+
+// But if the type of the expected elements is specified, then the elements to append will have to be of the that type.
+let anotherArray: number[] = [];
+anotherArray[0] = 1;
+// This assignment will throw an error. It is always recommended to explicitly set an empty array's type.
+// anotherArray[1] = "2";
+
+// Code completion. The TS compiler also completes the methods for the expected type of the array's elements.
+// anotherArray.forEach(n => n.);
